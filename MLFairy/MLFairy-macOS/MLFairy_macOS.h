2cc8e0abb863d6199dca17d30dc8ee40c20c31ef
MLFairy <mlfairy@vijaysharma.ca>
2019-08-28 12:59:12 -0400

    Refactor project structure. No more git submodules.

---
+++ MLFairy/MLFairy-macOS/MLFairy_macOS.h
@@ -0,0 +1,19 @@
+//
+//  MLFairy_macOS.h
+//  MLFairy macOS
+//
+//  Created by Vijay Sharma on 2019-08-28.
+//  Copyright © 2019 MLFairy. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+//! Project version number for MLFairy_macOS.
+FOUNDATION_EXPORT double MLFairy_macOSVersionNumber;
+
+//! Project version string for MLFairy_macOS.
+FOUNDATION_EXPORT const unsigned char MLFairy_macOSVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <MLFairy_macOS/PublicHeader.h>
+
+
