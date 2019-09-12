48d7cc7bfcb1f8f3bd0e40dc013ad442c394055e
MLFairy <mlfairy@vijaysharma.ca>
2019-09-11 23:49:24 -0400

    Improved demo app skeleton

--- MLFairyApp.iOS/MLFairyApp.iOS/ViewController.swift
+++ MLFairyApp.iOS/MLFairyApp.iOS/ViewController.swift
@@ -11,68 +11,45 @@ import Promises
 
 class ViewController: UIViewController {
 	private let HEALTH_MODEL_TOKEN = "MLFcfcEL6JiOn5jMEKDsy6n"
-	private let HEALTH_MODEL_TOKEN_BROKEN = "TUwZTZhZjAtOTIzMS0xMWU5LWEwODItZGQwNWRmMTdjYzFjLzRhYjQ0NDUwLWE3ZTgtMTFlOS04NzNjLTc1MmE5YTZmOWI5Yg=="
+	
+	@IBOutlet weak var imageView: UIImageView!
+	@IBOutlet weak var outputLabel: UILabel!
 	
 	override func viewDidLoad() {
 		super.viewDidLoad()
 		
-		var button = UIButton(type: .roundedRect)
-		button.frame = CGRect(x: 50, y: 50, width: 100, height: 30)
-		button.setTitle("good", for: [])
-		button.addTarget(self, action: #selector(self.good(_:)), for: .touchUpInside)
-		view.addSubview(button)
+	}
+	
+	@IBAction func takePhoto(_ sender: Any) {
 		
-		button = UIButton(type: .roundedRect)
-		button.frame = CGRect(x: 50, y: 100, width: 100, height: 30)
-		button.setTitle("broken", for: [])
-		button.addTarget(self, action: #selector(self.broken(_:)), for: .touchUpInside)
-		view.addSubview(button)
 	}
 	
-	@IBAction func broken(_ sender: AnyObject) {
-		self.refresh(token: HEALTH_MODEL_TOKEN_BROKEN)
+	@IBAction func selectPhoto(_ sender: Any) {
+		
 	}
 	
-	@IBAction func good(_ sender: AnyObject) {
-		self.refresh(token: HEALTH_MODEL_TOKEN)
-	}
 	
-	private func refresh(token: String) {
-//		self.promiseExample()
-		MLFairy.getCoreMLModel(token) { model, error in
+	@IBAction func downloadLatest(_ sender: Any) {
+		self.outputLabel.text = "Downloading model from MLFairy..."
+		MLFairy.getCoreMLModel(HEALTH_MODEL_TOKEN) { model, error in
 			guard error == nil else {
+				self.outputLabel.text = "Failed to download CoreML model"
 				print("Failed to get CoreML model \(String(describing: error)).")
 				return
 			}
-
+			
 			guard let _ = model else {
+				self.outputLabel.text = "Failed to assign CoreML model"
 				print("Failed to get CoreML model.")
 				return
 			}
-
-			print("Model Downloaded")
-//			ml.model = model
+			
+			self.outputLabel.text = "Model successfully downloaded"
 		}
 	}
 	
-	func promiseExample() {
-		let computationQueue = DispatchQueue(label: "com.mlfairy.computation")
-		let requestQueue = DispatchQueue(label: "com.mlfairy.requestQueue")
-		let compilationQueue = DispatchQueue(label: "com.mlfairy.compilation", target: computationQueue)
+	private func refresh(token: String) {
 		
-		Promise<String>(on: requestQueue) { () -> String in
-			let result = try await(Promise<String>(on: computationQueue) { resolve, reject in
-				resolve("1")
-			})
-			print(result)
-			return try await(Promise<String>(on: compilationQueue) { resolve, reject in
-				resolve("2")
-			})
-		}.then(on:.main) { output in
-			print("\(output)")
-		}.catch(on:.main) { error in
-			print("\(error)")
-		}
 	}
 }
 
