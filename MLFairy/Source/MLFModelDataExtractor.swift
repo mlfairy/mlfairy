58d1b97cba42fdd05c22a56d38694ff6a48e7712
MLFairy <mlfairy@vijaysharma.ca>
2019-10-01 23:07:04 -0400

    Update MLFSupport

--- MLFairy/Source/MLFModelDataExtractor.swift
+++ MLFairy/Source/MLFModelDataExtractor.swift
@@ -17,103 +17,15 @@ class MLFModelDataExtractor {
 	}
 	
 	func modelInformation(model: MLModel) -> [String: String] {
-		var dictionary: [String: String] = [:]
-		
-		for (key, value) in model.modelDescription.metadata {
-			if let value = value as? String {
-				dictionary[key.rawValue] = value
-			}
-		}
-		
-		var counter = 0
-		model.modelDescription.inputDescriptionsByName.forEach { _, description in
-			self.describe(description) { key, value in
-				dictionary["input\(counter)_\(key)"] = value
-			}
-			counter += 1
-		}
-		
-		counter = 0
-		model.modelDescription.outputDescriptionsByName.forEach { _, description in
-			self.describe(description) { key, value in
-				dictionary["output\(counter)_\(key)"] = value
-			}
-			
-			counter += 1
-		}
-		
-		return dictionary
+		return self.support.describe(model: model)
 	}
 	
 	func convert(
 		input: MLFeatureProvider,
 		output: MLFeatureProvider
 	) -> (input:[String: [String: String]], output: [String: [String: String]]) {
-		var inputResult: [String: [String: String]] = [:]
-		self.iterate(over: input).forEach { item in
-			inputResult[item.0] = item.1
-		}
-		
-		var outputResult: [String: [String: String]] = [:]
-		self.iterate(over: output).forEach { item in
-			outputResult[item.0] = item.1
-		}
-		
+		let inputResult = self.support.encode(input: input)
+		let outputResult = self.support.encode(input: output)
 		return (input: inputResult, output: outputResult)
-	}
-	
-	private func iterate(over provider: MLFeatureProvider) -> [(String, [String: String])] {
-		return provider.featureNames
-			.map { name -> (name: String, feature:MLFeatureValue?) in
-				return (name: name, feature: provider.featureValue(for: name))
-			}.filter {
-				$0.feature != nil
-			}.map { input -> (String, [String: String]) in
-				let value = self.support.extract(input.feature!)
-				return (input.name, value)
-			}
-	}
-	
-	private func describe(
-		_ description: MLFeatureDescription,
-		callback: (String, String) -> Void
-	) {
-
-		callback("name", description.name)
-		callback("type", "\(description.type.rawValue)")
-		callback("optional", description.isOptional ? "1" : "0")
-		
-		var extras: [String: String] = [:]
-		if let constraint = description.dictionaryConstraint {
-			extras["dictionaryConstraint"] = "\(constraint.keyType.rawValue)"
-		}
-		
-		if let constraint = description.imageConstraint {
-			extras["imageConstraint"] = "\(constraint)"
-		}
-		
-		if let constraint = description.multiArrayConstraint {
-			extras["multiArrayConstraint"] = "\(constraint)"
-		}
-		
-		if #available(iOS 12.0, macOS 10.14, tvOS 12.0, *) {
-			if let constraint = description.sequenceConstraint {
-				extras["sequenceConstraint"] = "\(constraint)"
-			}
-		}
-		
-		if let extras = extras.asJsonString() {
-			callback("extras", extras)
-		}
-	}
-}
-
-extension Dictionary {
-	func asJsonString() -> String? {
-		guard let jsonData = try? JSONSerialization.data(withJSONObject: self, options: []) else {
-			return nil
-		}
-		
-		return String(data: jsonData, encoding: .utf8)
 	}
 }
